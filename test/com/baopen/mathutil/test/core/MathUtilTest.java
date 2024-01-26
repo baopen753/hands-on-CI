d0d32a79828d1a66940d390957469a8999ec1bf9
baopen753 <tranquocbao070503@gmail.com>
2024-01-26 07:59:57 +0700

    alter readme

--- test/com/baopen/mathutil/test/core/MathUtilTest.java
+++ test/com/baopen/mathutil/test/core/MathUtilTest.java
@@ -21,8 +21,6 @@ public class MathUtilTest {
     // @Test: JUnit sẽ phối hợp với JVM sẽ chạy hàm này
     // @Test phía hậu trường chính là public static void main()
     // có nhiều @Test ứng với nhiều case khác nhau để kiêm thử hàng tính giai thừa
-    
-    
     // C1: test với trả về là value tường minh
     @Test
     public void testGetFactorialGivenRightArgumentReturnsWell() // đặt tên hàm phải liên quan đến case cự thể mà test
@@ -46,15 +44,10 @@ public class MathUtilTest {
         // tương đương mệnh đề AND
     }
 
-    
-    
     // hàm getF() ta thiết kế có hai tình huống xử lý
     // 1. đưa data tử tế trong  [0...20]   --- done !
     // 2. đưa dữ liệu < 0 || > 20  ---- theo thiết kế của hàm thì sẽ throw Exception
     // ko dùng assertEquals() để so sánh 2 ngoại lệ
-    
-    
-    
     // C2: test khi trả về là exception
     // testcase
     // input: -5
@@ -65,13 +58,12 @@ public class MathUtilTest {
         MathUtil.getFactorial(-5);    // hàm @Test , hay hàm getF() chạy sẽ ném về ngoại lệ
 
     }
-    
+
     // C3: cách khác để bắt ngoại lệ, viết tự nhiên hơn
     // sử dụng lamda-expression  @FunctionalInterface: là Interface chỉ có duy nhất một hàm 
     // từ JDK 8 có thể viết body trong hàm của interface - nhưng phải tại hàm static hoặc default 
     // vì trong functional interface chỉ có 1 hàm, nhưng khi ta gọi hàm này thì việc gọi cả interface cấp phát vùng nhớ là điều ko cần thiết
     ///   ->> @FunctionalInterface sẽ hỗ trợ gọi hàm trong Interface mà ko cần phải khởi tạo Interface
-   
     @Test
     public void testGetFactorialGivenWrongArgumentReturnsExceptionLamdaVersion() {
         Assert.assertThrows(IllegalArgumentException.class,
@@ -82,12 +74,10 @@ public class MathUtilTest {
         //  MathUtil.getFactorial(-5);    // hàm @Test , hay hàm getF() chạy sẽ ném về ngoại lệ
     }
 
-    
-      // C4: chủ động kiểm soát ngoại lệ theo đúng message
+    // C4: chủ động kiểm soát ngoại lệ theo đúng message
     @Test
     public void testGetFactorialGivenWrongArgumentReturnsExceptionLamdaVersion_TryCatch() {
 
-      
         try {
             MathUtil.getFactorial(-10);
         } catch (Exception e) {
