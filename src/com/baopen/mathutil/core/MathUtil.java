68cd6f8e556cf7c1c58babb7077bbf2cd952ea19
baopen753 <tranquocbao070503@gmail.com>
2024-01-26 08:31:55 +0700

    update math util

--- src/com/baopen/mathutil/core/MathUtil.java
+++ src/com/baopen/mathutil/core/MathUtil.java
@@ -15,7 +15,7 @@ public class MathUtil {
     // hàm tính giai thừa !!!
     public static long getFactorial(int n) {
 
-        if (n < 0 || n > 3) {
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid arguments. N must be between 0...20");
         }
 
